ded16f01cee575c8c9778cc8c366bddfbe7e8300
lj428 <69785383+lj428@users.noreply.github.com>
2020-08-26 13:04:16 +1000

    Add files via upload

--- Assignmnet_1_Componenet_2_CODE/myscript.js
+++ Assignmnet_1_Componenet_2_CODE/myscript.js
@@ -29,6 +29,24 @@ function randomStringGenerator(string_length) {
 
 }
 
+function hole_counter(arr){
+
+	var counter = 0;
+	
+	arr.forEach((element, index, array) => {
+	
+		if (element in array === false){
+	
+			++counter;
+
+		}
+	
+	});
+
+	return counter;
+
+}
+
 function component2_1(){
 	
 	var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -125,7 +143,7 @@ function component2_1(){
 	//2-1(e)(ii)
 
 	var arr_pattern_e_ii = new Array(20);
-	var pattern_e_ii = /(?=^.{3,7}$)(?=.*\d)(?=.*[a-z]).*$/gi;
+	var pattern_e_ii = /(?=^.{3,7}$)(?=.*\d)(?=.*[a-z]).*$/gi;	
 	arr_strings.forEach((element, index, arr) => {
 	
 		if (!pattern_e_ii.test(element)){
@@ -164,16 +182,152 @@ function component2_1(){
 	console.log(arr_pattern_e_iii);
 	out += "<tr><td style='padding-bottom:50px;'>2-1(e)(iii):</td><td style='overflow:auto;'>" + JSON.stringify(arr_pattern_e_iii) + "</td></tr>";
 
-	/*
-	var pattern1 = 
-	var pattern2 = 
-	var pattern3 = 
-	var pattern4 = 
-	var pattern5 = 
-	var pattern6 = 
+	//2-1(e)(pattern-1)
 
-	*/
+	//Pattern 1 searches for any string with at least 3 digits, and starts with an 's'
+
+	var pattern1 = /^s(.*\d){3,}/gi;
+	var arr_pattern_1 = new Array(20);
+	arr_strings.forEach((element, index, arr) => {
+	
+		if (!pattern1.test(element)){
+		
+			arr_pattern_1[index] = "";
+
+		} else {
+		
+			arr_pattern_1[index] = element;
+
+		}
+
+	});
+
+	console.log(arr_pattern_1);
+	out += "<tr><td style='padding-bottom:50px;'>2-1(e)(pattern-1):</td><td style='overflow:auto;'>" + JSON.stringify(arr_pattern_1) + "</td></tr>";
+
+	//2-1(e)(pattern-2)
+
+	//Pattern 2 searches for any string between 4 and 9 elements long, and has at least one uppercase and one lowercase letter
+
+	var pattern2 = /(?=^.{4,9}$)((.*[a-z].*[A-Z].*)|(.*[A-Z].*[a-z].*)).*$/g;
+	var arr_pattern_2 = new Array(20);
+	arr_strings.forEach((element, index, arr) => {
+	
+		if (!pattern2.test(element)){
+		
+			arr_pattern_2[index] = "";
+
+		} else {
+		
+			arr_pattern_2[index] = element;
+
+		}
+
+	});
+
+	console.log(arr_pattern_2);
+	out += "<tr><td style='padding-bottom:50px;'>2-1(e)(pattern-2):</td><td style='overflow:auto;'>" + JSON.stringify(arr_pattern_2) + "</td></tr>";
+
+	//2-1(e)(pattern-3)
+
+	//Pattern 3 searches for strings that are made up entirely of digits and nothing else
+
+	var pattern3 = /\b\d+\b/g;
+	var arr_pattern_3 = new Array(20);
+	arr_strings.forEach((element, index, arr) => {
+	
+		if (!pattern3.test(element)){
+		
+			arr_pattern_3[index] = "";
+
+		} else {
+		
+			arr_pattern_3[index] = element;
+
+		}
+
+	});
+
+	console.log(arr_pattern_3);
+	out += "<tr><td style='padding-bottom:50px;'>2-1(e)(pattern-3):</td><td style='overflow:auto;'>" + JSON.stringify(arr_pattern_3) + "</td></tr>";
+
+	//2-1(e)(pattern-4)
+
+	//Pattern 4 searches for strings that are made up entirely of letters and nothing else
+
+	var pattern4 = /\b[a-z]+\b/gi;
+	var arr_pattern_4 = new Array(20);
+	arr_strings.forEach((element, index, arr) => {
+	
+		if (!pattern4.test(element)){
+		
+			arr_pattern_4[index] = "";
+
+		} else {
+		
+			arr_pattern_4[index] = element;
+
+		}
+
+	});
+
+	console.log(arr_pattern_4);
+	out += "<tr><td style='padding-bottom:50px;'>2-1(e)(pattern-4):</td><td style='overflow:auto;'>" + JSON.stringify(arr_pattern_4) + "</td></tr>";
+
+	//2-1(e)(pattern-5)
+
+	//Pattern 5 searches for strings that start with an uppercase letter and end with a lowercase letters
+
+	var pattern5 = /^[A-Z].*[a-z]$/g;
+	var arr_pattern_5 = new Array(20);
+	arr_strings.forEach((element, index, arr) => {
+	
+		if (!pattern5.test(element)){
+		
+			arr_pattern_5[index] = "";
+
+		} else {
+		
+			arr_pattern_5[index] = element;
+
+		}
+
+	});
+
+	console.log(arr_pattern_5);
+	out += "<tr><td style='padding-bottom:50px;'>2-1(e)(pattern-5):</td><td style='overflow:auto;'>" + JSON.stringify(arr_pattern_5) + "</td></tr>";
+
+	//2-1(e)(pattern-6)
+
+	//Pattern 6 looks for strings starting with either upper or lowercase 'c' or 'a', and has somewhere in the string two consectutive digits.
+
+	var pattern6 = /^(c|a)(?=.*\d\d).*$/gi;
+	var arr_pattern_6 = new Array(20);
+	arr_strings.forEach((element, index, arr) => {
+	
+		if (pattern6.test(element)){
+		
+			arr_pattern_6[index] = "";
+
+		} else {
+		
+			arr_pattern_6[index] = element;
+
+		}
+
+	});
+
+	console.log(arr_pattern_6);
+	out += "<tr><td style='padding-bottom:50px;'>2-1(e)(pattern-6):</td><td style='overflow:auto;'>" + JSON.stringify(arr_pattern_6) + "</td></tr>";
 
 	document.getElementById("component_2_1_out").innerHTML = out + "</table>";
+
+	//Array with holes
+
+	var arr_holes = [1, 2, 3, 4, , 6, , 8, , , 11, 12, , 14, , 16];
+
+	console.log(arr_holes);
+
+	console.log(hole_counter(arr_holes));
 
 }
