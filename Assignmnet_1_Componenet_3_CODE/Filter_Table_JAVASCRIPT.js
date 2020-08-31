eaccf8e5e8851177f6a36750335abdfd173d8572
Rafat2000 <53847001+Rafat2000@users.noreply.github.com>
2020-08-31 23:16:12 +1000

    Update Filter_Table_JAVASCRIPT.js

--- Assignmnet_1_Componenet_3_CODE/Filter_Table_JAVASCRIPT.js
+++ Assignmnet_1_Componenet_3_CODE/Filter_Table_JAVASCRIPT.js
@@ -3,9 +3,32 @@
 function priceSearch()
 
 function colourSearch() 
+// start coding
 
-var car = {type:"Jeep", price:20000, color:"black"};
-var car2 = {type:"Holden", price:1000, color:"red"};
-var car3 = {type:"Mazda", price:500, color:"blue"};
-var car4 = {type:"Fiat", price:15000, color:"white"};
+var cars = [
+    {
+        type:"Jeep",
+        price:20000,
+        color:"black"
+    },
+    {
+        type:"Holden",
+        price:1000,
+        color:"red"
+    },
+    {
+        type:"Mazda",
+        price:500,
+        color:"blue"
+    },
+    {
+        type:"Fiat",
+        price:15000,
+        color:"white"
+    },
+]
+
+
+var t = "<table>";
+for(var i = 0; i < )
       
