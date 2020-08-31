5addaca8cb28ddb67ef5043e7cfa149dc65bc98d
curtis48 <69785604+curtis48@users.noreply.github.com>
2020-08-31 23:06:49 +1000

    Update Filter_Table_JAVASCRIPT.js

--- Assignmnet_1_Componenet_3_CODE/Filter_Table_JAVASCRIPT.js
+++ Assignmnet_1_Componenet_3_CODE/Filter_Table_JAVASCRIPT.js
@@ -1,1 +1,7 @@
 // start coding
+
+var car = {type:"Jeep", price:20000, color:"black"};
+var car2 = {type:"Holden", price:1000, color:"red"};
+var car3 = {type:"Mazda", price:500, color:"blue"};
+var car4 = {type:"Fiat", price:15000, color:"white"};
+      
