ce8302b68ad08191de4903ab9f1c49621733261a
Rafat2000 <53847001+Rafat2000@users.noreply.github.com>
2020-09-01 19:53:53 +1000

    Add files via upload

---
+++ Filter_Table_JAVASCRIPT.js
@@ -0,0 +1,73 @@
+// start coding
+
+var cars = [
+    {
+        type:"Jeep",
+        price:20000,
+        color:"black"
+    },
+    {
+        type:"Holden",
+        price:1000,
+        color:"red"
+    },
+    {
+        type:"Mazda",
+        price:500,
+        color:"blue"
+    },
+    {
+        type:"Fiat",
+        price:15000,
+        color:"white"
+    },
+]
+
+
+var t = "";
+for(var i = 0; i < cars.length; i++)
+{
+    var tr = "<tr>";
+
+    tr += "<td>" + cars[i].type + "</td>";
+    tr += "<td>" + cars[i].price + "</td>";
+    tr += "<td>" + cars[i].color + "</td>";
+    tr += "</tr>";
+    t += tr;
+}
+
+
+document.getElementById("table").innerHTML = t;
+
+
+// functions
+function priceSearch()
+{
+    var price_input, filter, filter_table, table_row, table_data, output_value;
+
+    price_input = document.getElementById("myInput1");
+
+    filter = price_input.value.toUpperCase();
+
+    filter_table = document.getElementById("table")
+
+    table_row = filter_table.getElementsByTagName("tr");
+
+    for(var i = 0; i < table_row.length; i++)
+    {
+        table_data = table_row[i].getElementsByTagName("td")[1];
+        if(table_data)
+        {
+            output_value = table_data.innerText;
+
+            if(output_value.toUpperCase().indexOf(filter) > -1)
+            {
+                table_row[i].style.display = "";
+            }
+            else
+            {
+                table_row[i].style.display = "none";
+            }
+        }
+    }
+}
