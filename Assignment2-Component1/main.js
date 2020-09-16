611d636205465d3d3a93a4a42a9010ed19e70c0c
lj428 <69785383+lj428@users.noreply.github.com>
2020-09-16 19:23:52 +1000

    Add files via upload

--- Assignment2-Component1/main.js
+++ Assignment2-Component1/main.js
@@ -1,4 +1,3 @@
-
 //Task 1-4: Stacking Images
 
 let top_picture="animal-image-5";
@@ -81,13 +80,57 @@ $("#oddButton").on("click", function(){
 });
 
   })
-  //Task 1-6: Count Doubles INCOMPLETE
-  String.prototype.CountDouble=function(){
-      var count=0;
-      for (var i=0;i<this.length;i++){
-          if(this.charAt(i)==this.charAt(i+1)){
-              count++;
-          }
-      }
-      return count;
-  }
+
+//Task 1-7)
+
+String.prototype.countDoubles = function(){
+
+    var amount_of_doubles = 0;
+
+    for (var i = 0; i < this.length; i++){
+    
+        if (this.charAt(i) === this.charAt(i + 1))
+            ++amount_of_doubles;
+
+	}
+
+    return amount_of_doubles;
+
+}
+
+//Task 1-3
+
+function times(){
+
+    console.log(this.a);
+    console.log(this.b);
+
+    return this.a * this.b * 2 * 9;
+
+}
+
+ function times_click(){
+
+        var my_nums = {a:3, b:4};
+
+        $("#call").text(times.call(my_nums));
+        $("#apply").text(times.apply(my_nums));
+
+        var times_v2 = times.bind(my_nums);
+
+        $("#bind").text(times_v2());
+
+}
+
+$(function() {
+
+    $("#count_doubles_click").on('click', function(){
+    
+        $("#count_doubles_out").text($("#count_doubles").val().countDoubles());
+
+
+	});
+
+    $("#submit_nums").on('click', times_click);
+
+});
